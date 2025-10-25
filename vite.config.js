185f49da589fd4b0090e9d8e9568103e3d9219ec
Joseph Vigil <joeyvigil109329@gmail.com>
2025-10-24 18:46:09 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/vite-react-gh-pages/' // Set the base path for GitHub Pages deployment
+})
